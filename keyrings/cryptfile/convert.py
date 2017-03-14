c05a5008cb6387aa623ae66eb446f5ebe20dc0f8
Hans-Peter Jansen <hpj@urpla.net>
2017-03-14 14:49:15 +0100

    convert.py: verbose, keep and force options
    catch ValueError from invalid passwords
    implement keep, force and verbose options

--- keyrings/cryptfile/convert.py
+++ keyrings/cryptfile/convert.py
@@ -24,22 +24,34 @@ class CommandLineTool(object):
     def __init__(self):
         self.aesmodes = CryptFileKeyring._get_mode()
         self.parser = argparse.ArgumentParser(
-                        usage = '%(prog)s [-h] newmode [infile] [outfile]',
+                        usage = '%(prog)s [-hvkf] aesmode [infile] [outfile]',
                         epilog = NOTE,
                         formatter_class=argparse.RawDescriptionHelpFormatter)
-        self.parser.add_argument('newmode', help = 'new AES mode [one of: %s]' %
+        self.parser.add_argument('aesmode', help = 'new AES mode [one of: %s]' %
                                  ', '.join(self.aesmodes))
         self.parser.add_argument('infile', help = 'File to convert',
-                                 nargs = '?', default = None)
+                                 nargs = '?')
         self.parser.add_argument('outfile',
-                                 help = 'Converted file [default: infile + pid]',
-                                 nargs = '?', default = None)
+                                 help = 'Converted file [default: infile.pid]',
+                                 nargs = '?')
+        self.parser.add_argument('-v', '--verbose',
+                                 help = 'verbose mode (cumulative)',
+                                 action = 'count')
+        self.parser.add_argument('-k', '--keep',
+                                 help = 'keep old password',
+                                 action = 'store_true')
+        self.parser.add_argument('-f', '--force',
+                                 help = 'replace existing outfile',
+                                 action = 'store_true')
 
     def run(self, argv):
         args = self.parser.parse_args(argv)
 
+        self.setup_logging(args.verbose)
+
         inkr = CryptFileKeyring()
         outkr = CryptFileKeyring()
+        outkr.aesmode = args.aesmode
 
         # prepare infile
         infile = args.infile
@@ -55,44 +67,66 @@ class CommandLineTool(object):
         if not inkr._check_file():
             self.parser.exit(3, 'Failed to parse %s\n' % infile)
 
+        log.info('infile %s: %s', infile, inkr.scheme)
+
         # prepare infile
         outfile = args.outfile
         if not outfile:
             outfile = infile + '.%d' % os.getpid()
 
         if os.path.exists(outfile):
-            self.parser.exit(3, '%s exists already\n' % outfile)
+            if args.force:
+                os.remove(outfile)
+                log.info('%s removed', outfile)
+            else:
+                self.parser.exit(3, '%s exists already\n' % outfile)
 
         outkr.file_path = outfile
         outkr.filename = os.path.basename(outfile)
 
+        log.info('outfile %s: %s', outfile, outkr.scheme)
+
         # unlock the infile keyring
-        inkr.keyring_key
+        try:
+            inkr.keyring_key
+        except ValueError as e:
+            self.parser.exit(3, 'Unlock %s: %s\n' % (infile, e))
+
+        if args.keep:
+            outkr._get_new_password = lambda: inkr.keyring_key
+        else:
+            outkr.keyring_key
 
         config = configparser.RawConfigParser()
         config.read(infile)
 
-        log.info('infile %s: %s', infile, inkr.scheme)
-        log.info('outfile %s: %s', outfile, outkr.scheme)
-
         for section in config.sections():
+            log.debug('process section: %s', section)
             if section != escape('keyring-setting'):
                 for username in config.options(section):
                     username = unescape(username)
                     section = unescape(section)
                     log.info('process: %s.%s', section, username)
-                    passwd = inkr.get_password(section, username)
-                    if passwd:
-                        outkr.set_password(section, username, passwd)
+                    password = inkr.get_password(section, username)
+                    if password:
+                        outkr.set_password(section, username, password)
+                        log.debug('%s.%s: %s', section, username,  password)
                     else:
                         log.error('invalid entry: [%s]%s', section, username)
+
+        return 0
+
+    def setup_logging(self, verbose):
+        for idx, loglevel in enumerate(range(logging.WARNING,
+                                             logging.NOTSET,
+                                             -10)):
+            if idx == verbose:
+                break
+        logging.basicConfig(level = loglevel)
 
 
 def main(argv=None):
     """Main command line interface."""
-
-    logging.basicConfig(level = logging.DEBUG)
-
     if argv is None:
         argv = sys.argv[1:]
 
