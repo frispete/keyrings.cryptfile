16c57842c23cd9f48f102c51fd98d74da2e04ccc
Kyle Altendorf <sda@fstab.net>
2022-10-26 17:26:15 -0400

    hasattr hack for now

--- keyrings/cryptfile/file_base.py
+++ keyrings/cryptfile/file_base.py
@@ -92,7 +92,8 @@ class Keyring(FileBacked, KeyringBackend):
         if os.path.exists(self.file_path):
             config.read(self.file_path)
 
-        self._check_version(config)
+        if hasattr(self, "_check_version"):
+            self._check_version(config)
         assoc = self._generate_assoc(service, username)
         service = escape_for_ini(service)
         username = escape_for_ini(username)
