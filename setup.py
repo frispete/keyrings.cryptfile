12521233f2fd7d935c86e0621645d2a6c119e4fc
Kyle Altendorf <sda@fstab.net>
2022-10-27 12:14:40 -0400

    Expand CPython support classifiers to cover testing up through 3.11

--- setup.py
+++ setup.py
@@ -39,7 +39,11 @@ setup_params = dict(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     entry_points = {
         'keyring.backends': [
